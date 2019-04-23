7937889b08569ae91135d339d01f2fa1729bdf9f
Lorenzo Morelli <lorenzomorelli95@gmail.com>
2019-04-23 09:40:08 +0200

    variuos change: introduced another kmeans algorithm.

--- Progetto32/kmeans.h
+++ Progetto32/kmeans.h
@@ -2,13 +2,12 @@
 // Ethan Brodsky
 // October 2011
 
-void kmeans(
-            int  dim,		                     // dimension of data 
+void kmeans(int dim,		                     // dimension of data
 
-            double *X,                        // pointer to data
-            int   n,                         // number of elements
-            
-            int   k,                         // number of clusters
-            double *cluster_centroid,         // initial cluster centroids
-            int   *cluster_assignment_final  // output
-           );
+		double *X,                        // pointer to data
+		int n,                         // number of elements
+
+		int k,                         // number of clusters
+		double *cluster_centroid,         // initial cluster centroids
+		int *cluster_assignment_final  // output
+		);
